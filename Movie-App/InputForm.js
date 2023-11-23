4b0c953ddc6a89f253e81ad229d1d5195ff17cf4
E.Bruhin.inf21 <e.bruhin.inf21@stud.bbbaden.ch>
2023-11-23 08:45:35 +0100

    InputForm

---
+++ Movie-App/InputForm.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { View, TextInput, Text, Button, StyleSheet } from 'react-native';
+
+export default function InputForm() {
+  const [filmName, setFilmName] = useState();
+  const [description, setDescription] = useState();
+
+  const handleFilmNameChange = (text) => {
+    setFilmName(text);
+  };
+
+  const handleDescriptionChange = (text) => {
+    setDescription(text);
+  };
+
+  const handleSubmit = () => {
+    console.log('Film Name:', filmName);
+    console.log('Description:', description);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter film name"
+        value={filmName}
+        onChangeText={handleFilmNameChange}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Description"
+        value={description}
+        onChangeText={handleDescriptionChange}
+      />
+      <Button title="Submit" onPress={handleSubmit} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    width: '100%',
+    marginBottom: 20,
+  },
+});
