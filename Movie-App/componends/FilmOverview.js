eb3340053123abdcd953d7fee7e3c6a9826c49bc
Egloff Nicola BBB <89132258+Nicola3341246@users.noreply.github.com>
2023-11-30 08:14:58 +0100

    Added design

--- Movie-App/componends/FilmOverview.js
+++ Movie-App/componends/FilmOverview.js
@@ -1,5 +1,4 @@
 import { Text, View, StyleSheet, Pressable, Image } from "react-native";
-import TestPoster from "../assets/img/TestPoster.jpg";
 
 export default function FilmOverview({ film, back, deleteFilm, editFilm }) {
     if (film == undefined || film.title === undefined) {
@@ -16,7 +15,7 @@ export default function FilmOverview({ film, back, deleteFilm, editFilm }) {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>{film.title}</Text>
-            <Image source={filmPoster} style={styles.filmPoster} />
+            <Image source={film.poster} style={styles.filmPoster} />
             <Text style={styles.description}>{film.description}</Text>
             <View style={styles.bottomBar}>
                 <Pressable style={styles.button} onPress={deleteFilm}>
@@ -40,6 +39,9 @@ const styles = StyleSheet.create({
 
         width: "100%",
         height: "100%",
+
+        fontFamily: "arial",
+        backgroundColor: "#6b6b6b",
     },
 
     title: {
@@ -59,7 +61,7 @@ const styles = StyleSheet.create({
     bottomBar: {
         display: "flex",
         flexDirection: "row",
-        justifyContent: "space-around",
+        justifyContent: "Space-Between",
 
         position: "absolute",
         bottom: 0,
@@ -67,8 +69,12 @@ const styles = StyleSheet.create({
     },
 
     button: {
-        backgroundColor: "grey",
+        width: "33%",
+        margin: "0",
         padding: "0.5rem",
-        width: "15%",
+        borderRadius: "0.5rem 0 0.5rem 0",
+
+        textAlign: "center",
+        backgroundColor: "#e01616",
     },
 });
